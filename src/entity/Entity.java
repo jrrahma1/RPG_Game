a86f539a794f0a6dcae8b236fd8b0deab517523d
Jeshad <166292174+jrrahma1@users.noreply.github.com>
2025-06-01 21:46:46 -0700

    character movement finished

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,7 +1,16 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
 	public int x, y;
 	public int speed;
+
+	public BufferedImage up1, up2, upStill, down1, down2, downStill, left1, left2, leftStill, right1, right2, rightStill;
+	public String direction;
+
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
+	boolean moving = false;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,14 +1,17 @@
 package entity;
 
-import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
-import java.awt.Graphics2D;
 
 public class Player extends Entity{
 
-	GamePanel gp; 
+	GamePanel gp;
 	KeyHandler keyH;
 	
 	public Player(GamePanel gp, KeyHandler keyH) {
@@ -16,31 +19,95 @@ public class Player extends Entity{
 		this.keyH = keyH;
 		
 		setDefaultValues();
+		getPlayerImage();
 		
 	}
 	public void setDefaultValues() {
 		x = 100;
 		y = 100;
 		speed = 4;
-		
+		direction = "down";
+	}
+
+	public void getPlayerImage() {
+		try {
+
+			up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/up1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/up2.png"));
+			upStill = ImageIO.read(getClass().getResourceAsStream("/res/player/upStill.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/down1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/down2.png"));
+			downStill = ImageIO.read(getClass().getResourceAsStream("/res/player/downStill.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/right1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/right2.png"));
+			rightStill = ImageIO.read(getClass().getResourceAsStream("/res/player/rightStill.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/left1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/left2.png"));
+			leftStill = ImageIO.read(getClass().getResourceAsStream("/res/player/leftStill.png"));
+
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public void update() {
+		moving = false;
 		if (keyH.upPressed == true) {
+			direction = "up";
 			y -= speed;
+			moving = true;
 		}
 		else if (keyH.downPressed == true) {
+			direction = "down";
 			y += speed;
+			moving = true;
 		}
 		else if (keyH.leftPressed == true) {
+			direction = "left";
 			x -= speed;
+			moving = true;
 		}
 		else if (keyH.rightPressed == true){
+			direction = "right";
 			x += speed;
+			moving = true;
+		}
+
+		if (moving) {
+		spriteCounter++;
+		if (spriteCounter > 10) {
+			spriteNum = (spriteNum == 1) ? 2 : 1;
+			spriteCounter = 0;
+		}
+		} else {
+			spriteNum = 0; // standing still
 		}
 	}
 	public void draw(Graphics2D g2) {
-		g2.setColor(Color.white);
-		
-		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+		BufferedImage image = null;
+
+		switch (direction) {
+			case "up":
+				if (spriteNum == 0) image = upStill;
+				else if (spriteNum == 1) image = up1;
+				else if (spriteNum == 2) image = up2;
+				break;
+			case "down":
+				if (spriteNum == 0) image = downStill;
+				else if (spriteNum == 1) image = down1;
+				else if (spriteNum == 2) image = down2;
+				break;
+			case "left":
+				if (spriteNum == 0) image = leftStill;
+				else if (spriteNum == 1) image = left1;
+				else if (spriteNum == 2) image = left2;
+				break;
+			case "right":
+				if (spriteNum == 0) image = rightStill;
+				else if (spriteNum == 1) image = right1;
+				else if (spriteNum == 2) image = right2;
+				break;
+		}
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 	}
 }
+
